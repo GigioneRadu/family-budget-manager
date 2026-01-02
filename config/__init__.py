2cbeee1deccc341b1ed8d490c35a19fda2cdd26b
Gheorghe Cintar <gheorghe.cintar@gmail.com>
2026-01-02 21:58:12 +0200

    Initialize config package with imports and exports

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,14 @@
+"""Config package initialization"""
+
+from .database import get_connection, init_database
+from .categories import BUDGET_CATEGORIES, CATEGORY_COLORS, CATEGORY_ICONS, get_income_categories
+
+__all__ = [
+    'get_connection',
+    'init_database',
+    'BUDGET_CATEGORIES',
+    'CATEGORY_COLORS',
+    'CATEGORY_ICONS',
+    'get_income_categories'
+]
 
